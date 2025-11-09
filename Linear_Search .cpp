f1178a45ab000eb3b8056e27a2f462d9f840009a
Smita Paramita Shreya <82666203+smita2005@users.noreply.github.com>
2025-11-09 08:06:40 +0600

    Create Linear_Search .cpp

---
+++ Linear_Search .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int found = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = i;
+            break;
+        }
+    }
+
+    if(found != -1)
+        cout << "Element found at index " << found << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
